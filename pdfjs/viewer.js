a4f48f68db13dce6064c14b5ca30713d588b9810
tarun261003 <122869742+tarun261003@users.noreply.github.com>
2024-03-29 20:05:51 +0530

    Update viewer.js

--- pdfjs/viewer.js
+++ pdfjs/viewer.js
@@ -27,7 +27,7 @@
 
 'use strict';
 
-var DEFAULT_URL =  "Introduction to Information Visualization.pdf";
+var DEFAULT_URL =  "DVT UNIT 5.pdf";
 var DEFAULT_SCALE_DELTA = 1.1;
 var MIN_SCALE = 0.25;
 var MAX_SCALE = 10.0;
